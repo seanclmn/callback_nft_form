0d2e4e3e675c59a99bce9ac86fc439b5c5282f7a
secolema <seanclmn1@gmail.com>
2022-12-04 17:51:05 +0900

    Started form

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css"
+import { useState, FormEvent} from "react"
+
+interface formModel {
+	name: string
+	description: string
+	partner: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	const initialFormState: formModel= {				
+		name: "",
+		description: "",
+		partner: ""
+	}
+
+	const submitForm = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		console.log(formState)
+	}
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormState(values => ({...values, [name]: value}))
+	}
+
+	const [formState,setFormState]=useState(initialFormState)
+  
+	return (
+		<form onSubmit={(e)=>submitForm(e)}>
+			<input 
+				placeholder="NFT NAME" 
+				name="name"
+				value={formState.name || ""}
+				onChange={handleChange}
+				/>
+			<br/>	
+
+			<input 
+				placeholder="NFT DESCRIPTION"
+				name="description"
+				value={formState.description || ""}
+				onChange={handleChange}
+				/>
+			<br/>
+		
+			<input id="partner" 
+				placeholder="PARTNER NAME"
+				name="partner"
+				value={formState.partner || ""}
+				onChange={handleChange}
+				/>
+			<br/>
+		
+			<button type="submit">SUBMIT</button>
+		</form>
+	)
 }
 
 export default App
